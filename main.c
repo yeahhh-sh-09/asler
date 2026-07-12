8add8dab50334a39dcaf3d0b785b54d5c49dacb7
Yash <yeahhhsh09@gmail.com>
2026-07-12 18:59:51 +0530

    removed un-needed arrays for now ( and also fixed 12 bugs )

--- main.c
+++ main.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-
-
 int CheckIn(char input, char array[] ){
 	int is_exists ;
 	int ArrayLen = sizeof(*array)/sizeof(array[0]) ;
@@ -17,28 +15,35 @@ int CheckIn(char input, char array[] ){
 	return is_exists ;
 }
 
+int GetFlow(char ch ){
+	int flow ;
+	if( ch>='A' || ch<='Z' || ch>='0' || ch<='9' || ch>='a' || ch<='z'){
+		flow = 0 ;
+	}
+	else if ( CheckIn(ch,operators)==1){
+		flow = 1 ;
+	}
+	else{
+		flow = -1 ;
+	}
+
+	return flow ;
+}
+
 int main(){ // this main() will later be converted into lexer() function and this main.c will convert into lexer.h or lexer.c that will be used in later main.c
 	
 	char *tokens[] = {};
 	FILE *code = fopen("sample.txt" , "r") ;
 
-	char brackets[] = {'[' , ']' ,'(' , ')' , '{', '}'} ;
-	char strings[] = {'"' , "'"  } ;
 	char operators[] = {'+' , '-' , '*' , '/' , '%' , '=' , '<' , '>' , '!' } ;
-	char special_operators[] = {'#' , '$' , '&' , '|' } ;
-	char signs[] = {'[' , ']' ,'(' , ')' , '{', '}' , 
-			'"' , "'" ,  
-		       	'+' , '-' , '*' , '/' , '%' , '=' , '<' , '>','!' , 
-			'#' , '$' , '&' , '|' } ;
-	
 	
 	char temp[] = {} ;
 	int i = 0 ;
 	int LenCode = sizeof(*code)/sizeof(code[0]) ;
-
+	
 	while(fgetc(code)!=EOF){
 		int flow ; // flow = 0 for AZaz09_ and flow = 1 for operators and for character in signs but not in operators will make flow = -1 change in flow will append characters stored in temp[] will append in tokens[] .. rest if it is in signs but not in operators.. so it will directly append in tokens[]   
 		char ch=fgetc(code) ;
-		
+		flow = GetFlow(ch) ;
 	}
 }
